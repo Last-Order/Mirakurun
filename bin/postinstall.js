d9875cee8d5676d0a7a136300b3ffacce218d4e4
Eridanus Sora <sora@sound.moe>
2023-04-13 00:41:33 +0800

    force service name

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -113,6 +113,7 @@ if (process.platform === "linux" || process.platform === "darwin") {
         "winser.cmd",
         [
             "-i", "-a", "--startuptype", "auto",
+            "-n", "mirakurun",
             "--startcmd", `node.exe bin\\init.win32.js`,
             "--set", "AppPriority ABOVE_NORMAL_PRIORITY_CLASS",
             "--set", "Type SERVICE_WIN32_OWN_PROCESS",
